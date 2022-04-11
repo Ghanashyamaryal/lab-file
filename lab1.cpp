199cf6f70a1ebab49c15e45695717d5307134da6
Anjan <anjanaryal13@gmail.com>
2022-04-11 14:54:00 +0545

    "lab 1"

---
+++ lab1.cpp
@@ -0,0 +1,9 @@
+//to show the concept of endl and setw
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int sum;
+    cout<<"setw(0)="<<"\n"<<setw(0)<<sum<<endl;
+    cout<<"setw(20)="<<"\n"<<setw(20)<<sum<<endl;
+}
